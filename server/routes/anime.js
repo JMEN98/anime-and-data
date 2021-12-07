f916196fecdae6d5769b0bcf90b6df803066d1b6
JMEN98 <78163678+JMEN98@users.noreply.github.com>
2021-12-07 17:21:01 -0500

    router

--- server/routes/anime.js
+++ server/routes/anime.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Anime } from '../models'
 
 
 const router=express.Router()
@@ -6,6 +7,21 @@ const router=express.Router()
 router.get('/', (req, res, next) => {
   res.status(200).send('api endpoint')
 
+})
+router.post('/anime',(req,res,next)=> {
+const [title,year,genres,maincharater]=req.boby
+const post = new post({
+title:title,
+year:year,
+genres:genres,
+maincharater:maincharater
+
+
+
+})
+
+const saveAnime =await post.save()
+res.json(saveAnime.toJson())
 })
 
 
