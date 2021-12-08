9baedb41f1eb34669bdcf3c1804e860b0ca41ee4
JMEN98 <78163678+JMEN98@users.noreply.github.com>
2021-12-08 12:11:38 -0500

    some-more-routers

--- server/routes/anime.js
+++ server/routes/anime.js
@@ -5,9 +5,19 @@ import { Anime } from '../models'
 const router=express.Router()
 
 router.get('/', (req, res, next) => {
-  res.status(200).send('api endpoint')
+  res.status(200).send('api endpoint Anime')
 
 })
+
+router.get('/album',  async (req, res, next) => {
+  let anime = await Anime.find({}).sort({ title:1}).exec();
+ 
+ 
+   console.log(anime)
+   
+   
+   res.send(anime)
+ })
 router.post('/anime',(req,res,next)=> {
 const [title,year,genres,maincharater]=req.boby
 const post = new post({
@@ -22,7 +32,7 @@ maincharater:maincharater
 
 const saveAnime =await post.save()
 res.json(saveAnime.toJson())
-})
+});
 
 
 module.exports = router
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import react, { useEffect, useState } from "react";
-// import Anime from "../Anime.json"
+//import Anime from "../Anime.json"
 
 function Home (){
   const [Anime,SetAnime]=useState([])
 
   useEffect(function(){
-    fetch("http://localhost:3000/api/anime")
+    fetch("http://localhost:3000/api/Anime")
     .then(response=>response.json()).then(data=>{
       SetAnime(data)
       console.log(data) 
